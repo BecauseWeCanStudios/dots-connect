3fa7c0ba0cb804e03159d7e52f9f137eb75b198c
GoldFeniks <ggoldenfeniks@gmail.com>
2016-07-13 20:59:18 +1000

    [MENU] Add auto-resize

--- menu.js
+++ menu.js
@@ -17,10 +17,15 @@ class Menu {
         this.game = game;
         this.levelsCount = storage.levelCount() + 1;
         this.state = MenuStates.MAIN_MENU;
+        this.parent.style.width = this.parent.style.height = 
+            Math.ceil(Math.min(window.innerHeight, window.innerWidth)) * 0.8 + 'px';
+        window.onresize = this.updateAll.bind(this);
         this.createMenuDiv(parent);
     }
 
     static setElementStyle(element, style) {
+        if (!element)
+            return;
         let s = '';
         for (let i = 0; i < style.length; ++i)
             s += style[i][0] + ': ' + style[i][1] + '; ';
@@ -28,6 +33,8 @@ class Menu {
     }
 
     static setElementAttributes(element, attributes) {
+        if (!element)
+            return;
         for (let i = 0; i < attributes.length; ++i)
             element.setAttribute(attributes[i][0], attributes[i][1]);
     }
@@ -45,46 +52,56 @@ class Menu {
             element.addEventListener(listeners[i][0], listeners[i][1]);
     }
 
+    updateMenuDiv() {
+        let titleFontSize = Math.floor(this.parent.offsetHeight * 0.15),
+            buttonFontSize = Math.floor(this.parent.offsetHeight * 0.08);
+        Menu.setElementStyle(this.menuDiv, [
+            ['line-height', titleFontSize + 'px'],
+            ['font-size', titleFontSize + 'px']
+        ]);
+        Menu.setElementStyle($('new-game-button'), [
+            ['line-height', buttonFontSize + 'px'],
+            ['font-size', buttonFontSize + 'px'],
+            ['height', (buttonFontSize * 1.2 | 0) + 'px']
+        ]);
+        Menu.setElementStyle($('score-board-button'), [
+            ['line-height', buttonFontSize + 'px'],
+            ['font-size', buttonFontSize + 'px'],
+            ['height', (buttonFontSize * 1.2 | 0) + 'px']
+        ]);
+    }
+
     createMenuDiv() {
-        let titleFontSize = this.parent.offsetHeight * 0.15 | 0,
-            buttonFontSize = this.parent.offsetHeight * 0.09 | 0;
         this.menuDiv = Menu.createElement('div', [['id', 'menu-div']], [], '');
-        this.menuDiv.appendChild(Menu.createElement('label', [['id', 'title-label']], [
-                ['line-height', titleFontSize + 'px'],
-                ['font-size', titleFontSize + 'px']
-            ], 'DOTS CONNECT'
-        ));
-        let button = Menu.createElement('button', [['id', 'new-game-button'], ['class', 'glowEnabledGreen']], [
-                ['line-height', buttonFontSize + 'px'],
-                ['font-size', buttonFontSize + 'px'],
-                ['height', (buttonFontSize * 1.2 | 0) + 'px']
-            ], 'NEW GAME'
-        );
+        this.menuDiv.appendChild(Menu.createElement('label', [['id', 'title-label']], [], 'DOTS CONNECT'));
+        let button = Menu.createElement('button', [['id', 'new-game-button'], ['class', 'glowEnabledGreen']], [], 'NEW GAME');
         Menu.assignListeners(button, [['click', this.newGameClick.bind(this)]]);
         this.menuDiv.appendChild(button);
-        this.menuDiv.appendChild(Menu.createElement('button', [['id', 'score-board-button'], ['class', 'glowEnabledGreen']], [
-                ['line-height', buttonFontSize + 'px'],
-                ['font-size', buttonFontSize + 'px'],
-                ['height', (buttonFontSize * 1.2 | 0) + 'px']
-            ], 'SCORE BOARD'
-        ));
+        this.menuDiv.appendChild(Menu.createElement('button', [['id', 'score-board-button'], ['class', 'glowEnabledGreen']],
+            [], 'SCORE BOARD'));
         Menu.assignListeners(this.menuDiv, [['transitionend', this.menuDivTransitionEnd.bind(this)]]);
         Menu.assignListeners(this.menuDiv, [['animationend', this.animationEnd.bind(this)]]);
         this.parent.appendChild(this.menuDiv);
+        this.updateMenuDiv();
     }
 
-    createBackButton() {
+    updateBackButton() {
         let buttonSize = Math.floor(this.parent.offsetWidth * 0.1);
-        this.backButton = Menu.createElement('button', [['id', 'back-button']], [
+        Menu.setElementStyle($('back-button'), [
             ['width', buttonSize + 'px'],
             ['height', buttonSize + 'px'],
             ['top', this.parent.offsetHeight - buttonSize + 'px'],
             ['left', this.parent.offsetWidth + buttonSize * 1.5 + 'px'],
             ['font-size', buttonSize * 0.5 + 'px'],
             ['line-height', buttonSize * 0.5 + 'px']
-        ], '✖');
+        ]);
+    }
+
+    createBackButton() {
+        this.backButton = Menu.createElement('button', [['id', 'back-button']], [], '✖');
         Menu.assignListeners(this.backButton, [['click', this.backButtonClick.bind(this)]]);
         $('main-container').appendChild(this.backButton);
+        this.updateBackButton();
     }
     
     sceneFadeAnimationEnd() {
@@ -164,37 +181,38 @@ class Menu {
         Menu.assignListeners(this.game.scene.canvas, [['animationend', this.sceneFadeAnimationEnd.bind(this)]]);
         this.game.scene.canvas.className = 'fadeIn';
     }
-    
-    createNextLevelButton() {
+
+    updateNextLevelButton() {
         let buttonSize = Math.floor(this.parent.offsetWidth * 0.1);
-        this.nextLevelButton = Menu.createElement('button', [['id', 'next-level-button']], [
-            //['position', 'relative'],
+        Menu.setElementStyle($('next-level-button'), [
             ['width', buttonSize + 'px'],
             ['height', buttonSize + 'px'],
             ['top', this.parent.offsetHeight - buttonSize * 4 + 'px'],
             ['left', this.parent.offsetWidth + buttonSize * 1.5 + 'px'],
             ['font-size', buttonSize * 0.5 + 'px'],
             ['line-height', buttonSize * 0.5 + 'px']
-        ], '→');
+        ]);
+    }
+
+    createNextLevelButton() {
+        this.nextLevelButton = Menu.createElement('button', [['id', 'next-level-button']], [], '→');
         Menu.assignListeners(this.nextLevelButton, [['click', this.nextLevelButtonClick.bind(this)]]);
         $('main-container').appendChild(this.nextLevelButton);
+        this.updateNextLevelButton();
     }
-    
-    createLevelButtons() {
+
+    updateLevelButtons() {
         let divSize = Math.floor(this.parent.offsetWidth * 0.9);
         let buttonsInRow = Math.ceil(Math.sqrt(this.levelsCount));
         let buttonsInCol = Math.ceil(this.levelsCount / buttonsInRow);
         let buttonSize = Math.ceil((divSize * 0.8) / buttonsInRow);
         let buttonsMargin = Math.ceil((divSize - buttonSize * buttonsInRow) / buttonsInRow - 1);
-        this.levelButtonsDiv = Menu.createElement('div', [['id', 'level-buttons-div']], [
+        Menu.setElementStyle(this.levelButtonsDiv, [
             ['height', buttonSize * buttonsInCol + buttonsMargin * (buttonsInCol - 1) + 'px'],
             ['width', buttonSize * buttonsInRow + buttonsMargin * (buttonsInRow - 1) + 'px']
-        ], '');
-        Menu.assignListeners(this.levelButtonsDiv, [['transitionend',this.buttonsDivTransitionEnd.bind(this)]]);
-        Menu.assignListeners(this.levelButtonsDiv, [['animationend',this.animationEnd.bind(this)]]);
-        this.parent.appendChild(this.levelButtonsDiv);
-        for (let i = 0; i < this.levelsCount; ++i) {
-            let button = Menu.createElement('button', [['id', i.toString()], ['class', 'level-select-button']], [
+        ]);
+        for (let i = 0; i < this.levelsCount; ++i) 
+            Menu.setElementStyle($(i.toString()), [
                 ['position', 'relative'],
                 ['width', buttonSize + 'px'],
                 ['height', buttonSize + 'px'],
@@ -202,10 +220,21 @@ class Menu {
                 ['left', buttonsMargin * (i % buttonsInRow)+ 'px'],
                 ['font-size', buttonSize * 0.5 + 'px'],
                 ['line-height', buttonSize * 0.5 + 'px']
-            ], i != this.levelsCount - 1 ? (i + 1).toString() : '?');
+            ]);        
+    }
+
+    createLevelButtons() {
+        this.levelButtonsDiv = Menu.createElement('div', [['id', 'level-buttons-div']], [], '');
+        Menu.assignListeners(this.levelButtonsDiv, [['transitionend',this.buttonsDivTransitionEnd.bind(this)]]);
+        Menu.assignListeners(this.levelButtonsDiv, [['animationend',this.animationEnd.bind(this)]]);
+        this.parent.appendChild(this.levelButtonsDiv);
+        for (let i = 0; i < this.levelsCount; ++i) {
+            let button = Menu.createElement('button', [['id', i.toString()], ['class', 'level-select-button']], 
+                [], i != this.levelsCount - 1 ? (i + 1).toString() : '?');
             Menu.assignListeners(button, [['click', this.levelButtonClick.bind(this)]]);
             this.levelButtonsDiv.appendChild(button);
         }
+        this.updateLevelButtons();
     }
 
     newGameClick() {
@@ -214,5 +243,19 @@ class Menu {
         this.state = MenuStates.LEVEL_SELECT;
         this.createBackButton();
         this.menuDiv.style.opacity = 0;
+    }
+    
+    static tryUpdate(object, func) {
+        if (object)
+            func();        
+    }
+    
+    updateAll() {
+        this.parent.style.width = this.parent.style.height =
+            Math.ceil(Math.min(window.innerHeight, window.innerWidth)) * 0.8 + 'px';
+        Menu.tryUpdate(this.menuDiv, this.updateMenuDiv.bind(this));
+        Menu.tryUpdate(this.backButton, this.updateBackButton.bind(this));
+        Menu.tryUpdate(this.nextLevelButton, this.updateNextLevelButton.bind(this));
+        Menu.tryUpdate(this.levelButtonsDiv, this.updateLevelButtons.bind(this));
     }
 }
